15a6907e4f273d6f0726e888262d143b238a34ea
Sean <yycyjqc@gmail.com>
2018-06-06 15:30:30 -0700

    scrape data and save as csv

---
+++ scrap.py
@@ -0,0 +1,31 @@
+
+# coding: utf-8
+# Dependencies
+from splinter import Browser
+from bs4 import BeautifulSoup
+import requests
+import pymongo
+import pandas as pd
+import csv
+
+# URL of page to be scraped
+data_url = "http://edu.sina.com.cn/gaokao/baoming/"
+data = pd.read_html(data_url)
+
+
+# Create DataFrame
+df = data[0]
+df.columns = ["省/市", '2017', '2016', '2015', '2014', '2013', '2012', '2011']
+df = df.drop([0, 1])
+
+
+# Remove unwanted symbols
+for column in df.columns:
+    df[column] = df[column].map(lambda x: x.lstrip('约超近').rstrip("↑↓+万万余"))
+
+
+# Reset the index to make sure it starts from 0
+df = df.reset_index(drop=True)
+
+# Save data as csv file
+df.to_csv("gaokao_population.csv")
